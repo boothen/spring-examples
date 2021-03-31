eb156aab2be7283d7182ddd4366d6b750f300dfa
Matt Garner <matt.garner@waitrose.co.uk>
2021-03-31 12:21:21 +0100

    Updated name.sh

--- spring-cloud-template/name.sh
+++ spring-cloud-template/name.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-find . -depth -name "*changeme*" -type d -execdir sh -c 'mv {} $(echo {} | sed "s/changeme/bar/")' \;
+if [ -n "$1" ]; then
+  echo "Naming project $1"
+else
+  echo "Project name is required"
+  exit 1
+fi
 
-find . -iname "*.gradle" -or -iname "*.java" -exec sed -i '' 's/changeme/bar/g' {} +
+find . -depth -name "*changeme*" -type d -execdir sh -c 'mv {} $(echo {} | sed "s/changeme/$1/")' \;
+
+find . -iname "*.gradle" -or -iname "*.java" | xargs sed -i '' "s/changeme/$1/g"
+
+echo "Finished"
